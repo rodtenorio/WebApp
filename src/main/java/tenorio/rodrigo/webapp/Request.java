d129903bd1ccc27895996c491caf54199782da6e
Rodrigo Tenorio <rtenorio@localhost.localdomain>
2015-09-13 12:41:03 -0300

    First commit

---
+++ src/main/java/tenorio/rodrigo/webapp/Request.java
@@ -0,0 +1,20 @@
+package tenorio.rodrigo.webapp;
+
+public class Request {
+
+	/*
+	 * Accept:
+Accept-Encoding:gzip, deflate
+Accept-Language:en-US,en;q=0.8,pt;q=0.6,pt-BR;q=0.4,es-419;q=0.2,es;q=0.2
+Connection:keep-alive
+Content-Length:37
+Content-Type:application/x-www-form-urlencoded
+Cookie:__utmt=1; __utma=173612345.1686919307.1442158422.1442158422.1442158422.1; __utmb=173612345.1.10.1442158422; __utmc=173612345; __utmz=173612345.1442158422.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)
+Host:www.kartodromogranjaviana.com.br
+Method:POST http://www.kartodromogranjaviana.com.br/live_timing/livetiming.php HTTP/1.1
+Origin:http://www.kartodromogranjaviana.com.br
+Referer:http://www.kartodromogranjaviana.com.br/live_timing/livetiming.php
+User-Agent:Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36
+	 */
+	
+}
