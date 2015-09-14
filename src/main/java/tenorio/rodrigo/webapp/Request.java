792d1842a36cc06493d38a072b95947758c14612
rtenorio <rodrigo.tenorio@oracle.com>
2015-09-14 11:49:57 -0300

    Apache HttpClient implemented.

--- src/main/java/tenorio/rodrigo/webapp/Request.java
+++ src/main/java/tenorio/rodrigo/webapp/Request.java
@@ -1,20 +1,47 @@
 package tenorio.rodrigo.webapp;
 
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+
 public class Request {
 
-	/*
-	 * Accept:
-Accept-Encoding:gzip, deflate
-Accept-Language:en-US,en;q=0.8,pt;q=0.6,pt-BR;q=0.4,es-419;q=0.2,es;q=0.2
-Connection:keep-alive
-Content-Length:37
-Content-Type:application/x-www-form-urlencoded
-Cookie:__utmt=1; __utma=173612345.1686919307.1442158422.1442158422.1442158422.1; __utmb=173612345.1.10.1442158422; __utmc=173612345; __utmz=173612345.1442158422.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)
-Host:www.kartodromogranjaviana.com.br
-Method:POST http://www.kartodromogranjaviana.com.br/live_timing/livetiming.php HTTP/1.1
-Origin:http://www.kartodromogranjaviana.com.br
-Referer:http://www.kartodromogranjaviana.com.br/live_timing/livetiming.php
-User-Agent:Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36
-	 */
-	
+    public static void main(final String[] args) throws Exception {
+        final CloseableHttpClient closeableHttpClient = HttpClients.createDefault();
+        final HttpPost httpPost = new HttpPost("http://www.kartodromogranjaviana.com.br/live_timing/livetiming.php");
+        httpPost.addHeader("Content-Type", "application/x-www-form-urlencoded");
+        // httpPost.addHeader("Accept-Language", "en-US,en;q=0.8,he;q=0.6");
+        // httpPost.addHeader("Origin",
+        // "http://www.kartodromogranjaviana.com.br");
+        // httpPost.addHeader("Referer",
+        // "http://www.kartodromogranjaviana.com.br/live_timing/livetiming.php");
+
+        final ArrayList<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("xajax", "InsertData"));
+        params.add(new BasicNameValuePair("xajaxr", Long.toString(new Date().getTime())));
+        final UrlEncodedFormEntity formEntity = new UrlEncodedFormEntity(params);
+        httpPost.setEntity(formEntity);
+
+        System.out.println(httpPost);
+        final CloseableHttpResponse closeableHttpResponse = closeableHttpClient.execute(httpPost);
+        final InputStream content = closeableHttpResponse.getEntity().getContent();
+        byte b;
+        while ((b = (byte) content.read()) != -1) {
+            System.out.print((char) b);
+        }
+
+        // DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        // dbf.setValidating(true);
+        // dbf.setIgnoringElementContentWhitespace(true);
+        // DocumentBuilder db = dbf.newDocumentBuilder();
+        // Document document = db.parse(content);
+    }
 }
